7da2db7e3f1557dca6be42aa945b5a83af46b4da
Ben Hoberg <benjhoberg@gmail.com>
2022-12-12 10:30:04 -0600

    JS challenge involving use of parameters.

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (numToQuarter) => {
+const quarter = numToQuarter / 4;
+return quarter;
+
+}
+
+// Invoke the function and store the return value
+const result = quarterValue(12);
+
+// Log the return value
+console.log(result);
